2186dddfcfe210a3b502a8dc2c2c536f73c783e0
jeongyeongjun <jeongyj789@naver.com>
2018-08-18 05:11:47 +0000

    z

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180818023255) do
+ActiveRecord::Schema.define(version: 20180818050817) do
 
   create_table "books", force: :cascade do |t|
     t.string   "book_title"
@@ -64,6 +64,11 @@ ActiveRecord::Schema.define(version: 20180818023255) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "profile_img"
+    t.integer  "sign_in_count"
+    t.datetime "current_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.datetime "last_sign_in_at"
+    t.string   "last_sign_in_ip"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
